272a21f55658d80b047f8c6aafa6f8cddfcbf7b5
csanchez015 <114633353+csanchez015@users.noreply.github.com>
2023-03-02 11:27:07 -0800

    Add files via upload

---
+++ ninjagold/src/test/java/com/christiansanchez/ninjagold/NinjagoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christiansanchez.ninjagold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjagoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
